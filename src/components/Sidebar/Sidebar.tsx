54a1a0c80aa2a40998986444f24f341264132b6d
Nitzan Cohen <nitzancohen@hotmail.com>
2026-01-25 23:56:11 +0200

    fix(sidebar): fixed sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,38 +1,15 @@
 import Link from "next/link";
 import styles from "./sidebar.module.css";
-import lessonHelper from "@/helpers/GetLessonNumber";
-import courseData from "@/data/course-web-development.json";
 
 type SidebarProps = {
   isVisibility: boolean;
 };
 
 function Sidebar({ isVisibility }: SidebarProps) {
-  const saveLessonNumber = lessonHelper.saveLessonNumer;
-  const courseContent = courseData.courseContent;
-
+  
   return (
     <section className={`${styles.container}`}>
       <Link href="/">חזרה לדף הבית</Link>
-      <h3 className={styles.title}>תוכן עניינים</h3>
-      <ul>
-        {courseContent.map((module, modIndex) => (
-          <li className={styles.moduleItem} key={modIndex}>
-            {module.title}
-            <ul>
-              {module.lessons.map((lesson, lesIndex) => (
-                <li
-                  className={styles.lessonItem}
-                  key={lesson.lessonId}
-                  onClick={() => saveLessonNumber(modIndex + 1, lesIndex + 1)}
-                >
-                  {lesson.title}
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
     </section>
   );
 }
